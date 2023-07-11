b59a0fb19726ffda47f2e6aad5d7779530cfc64d
itzakhil <upadhyayakhil07@gmail.com>
2023-07-11 14:35:25 +0530

    commit

--- Frontend Folder/src/context/appContext.js
+++ Frontend Folder/src/context/appContext.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 import React from "react";
-const SOCKET_URL = "https://chatapp-36oq.onrender.com/";
+const SOCKET_URL = "https://chatapp-36oq.onrender.com";
 export const socket = io(SOCKET_URL);
 // app context
 export const AppContext = React.createContext();
